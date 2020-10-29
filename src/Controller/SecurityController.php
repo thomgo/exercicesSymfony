2cf9bfa776851a26276ebc0d6cc71a59e30df3b4
Thomas <tg.59290@gmail.com>
2020-10-29 12:13:05 +0100

    fixed authentification issues du to upgrade to symfony 5

--- src/Controller/SecurityController.php
+++ src/Controller/SecurityController.php
@@ -21,4 +21,12 @@ class SecurityController extends AbstractController
 
         return $this->render('security/login.html.twig', ['last_username' => $lastUsername, 'error' => $error]);
     }
+
+    /**
+    * @Route("/logout", name="app_logout")
+    */
+     public function logout()
+     {
+         throw new \LogicException('This method can be blank - it will be intercepted by the logout key on your firewall.');
+     }
 }
